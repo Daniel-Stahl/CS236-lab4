fd3ca569290bc800c41f4377a23576666da7e4b4
Daniel Stahl <danny.stahl@gmail.com>
2020-10-09 15:01:30 -0700

    setting up project

---
+++ AVLTree.cpp
@@ -0,0 +1,9 @@
+//
+//  AVLTree.cpp
+//  Lab 4
+//
+//  Created by Daniel Stahl on 10/9/20.
+//  Copyright © 2020 Daniel Stahl. All rights reserved.
+//
+
+#include "AVLTree.hpp"
---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab 4
+//
+//  Created by Daniel Stahl on 10/9/20.
+//  Copyright © 2020 Daniel Stahl. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
